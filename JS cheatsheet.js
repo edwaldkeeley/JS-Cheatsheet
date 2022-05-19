9bae59fa2057e8496e2be589f6f945ac45f7ff0f
edwaldkeeley <edwaldpk@gmail.com>
2022-05-19 19:32:50 +0700

    first commit

---
+++ JS cheatsheet.js
@@ -0,0 +1,142 @@
+// @ts-nocheck
+
+// Variables
+// Const
+
+// Cant Change A Variable Declared With Const
+const x = 1;
+
+// Let
+
+// Useful If You Wanna Change The Variable
+let x = 1;
+
+// Var
+
+// Not Recommended To Be Used 
+var x = 1;
+
+///////////////////////////
+
+// Object
+
+// a Object That Store Stuff
+const Object = {
+  Stuff: "Random",
+  Stuff2: "Random2",
+};
+
+////////////////////////////
+// Array
+
+// Create a Array
+const Array = ["stuff", "stuff2"];
+
+///////////////////////////
+// Function
+
+// a Function
+function MyFunction(a, b) {
+  return a + b;
+}
+
+const result = MyFunction(2, 9);
+////////////////////////////
+// Math-Random
+
+//Create a Random Number
+console.log(Math.random());
+
+/////////////////////////////
+// Math-Trunc
+
+// Remove Decimal Number
+console.log(Math.trunc);
+
+/////////////////////////////
+// This Keyword
+
+// Refer To a Global Object
+const calcAge = function (birthYear) {
+  console.log(2022 - birthYear);
+  console.log(this);
+};
+
+calcAge(1991);
+
+/////////////////////////////
+// IF ELSE Statement
+
+const truly = true;
+//Only Execute If True
+if (truly) {
+  console.log("Is True");
+} else {
+  // Only Execute If False
+  console.log("Is False");
+}
+
+//////////////////////////////
+// For Loop
+
+// Loop Over Until It Reached The Limit
+for (let i = 0; i < 5; i++) {
+  console.log(`Test${i}`);
+}
+
+//////////////////////////////
+// While Loop
+
+while (i < 10) {
+  console.log("test");
+  i++;
+}
+
+///////////////////////////////
+// Arrow Function
+
+const myFunction = (a, b) => a * b;
+
+///////////////////////////////
+// Equality Operator
+
+// Strict Equality
+console.log(1 === 1);
+
+// Loose Equality
+// Not Recommended To Be Used 
+console.log(1 == '1');
+
+
+////////////////////////////////
+// Dates
+
+// Useful On Some Website
+const date = new Date();
+
+////////////////////////////////
+// Strict InEquality Operator
+
+console.log('1' !==  1);
+
+// Loose InEquality Operator
+
+console.log('1' !=  1);
+
+////////////////////////////////
+//Strict Equality Comparison Operator
+
+// If Equal Then True If Not Equal Then False
+console.log(1 === 1);
+
+//Loose Equality Comparison Operator
+
+console.log(1 == 1);
+
+/////////////////////////////////
+// Typeof
+
+// Typeof Show What DataType It Is
+console.log(typeof 'hello');
+
+////////////////////////////////
